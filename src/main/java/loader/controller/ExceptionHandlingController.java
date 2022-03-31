2fd5e22c7ca95e8271a6503803331f7a72ccbe92
Alex <alexeee33@gmail.com>
2022-03-31 19:07:29 +0700

    Now every text for task in variant can be formatted. There are 3 options: BOLD, ITALIC, UNDERLINED

--- src/main/java/loader/controller/ExceptionHandlingController.java
+++ src/main/java/loader/controller/ExceptionHandlingController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import javax.servlet.RequestDispatcher;
-
 @ControllerAdvice
 public class ExceptionHandlingController {
 
--- src/main/java/loader/controller/TeacherController.java
+++ src/main/java/loader/controller/TeacherController.java
@@ -40,16 +40,6 @@ public class TeacherController {
 
     @PostMapping("/newVariant")
     public ModelAndView newVariant(@ModelAttribute VariantForm variantForm) throws IOException, VariantNameExists {
-        String taskText1Edited = variantForm.getTaskText1().replace("\r\n", "\\n");
-        String taskText2Edited = variantForm.getTaskText2().replace("\r\n", "\\n");
-        String taskText3Edited = variantForm.getTaskText3().replace("\r\n", "\\n");
-        String taskText4Edited = variantForm.getTaskText4().replace("\r\n", "\\n");
-
-        variantForm.setTaskText1(taskText1Edited);
-        variantForm.setTaskText2(taskText2Edited);
-        variantForm.setTaskText3(taskText3Edited);
-        variantForm.setTaskText4(taskText4Edited);
-
         teacherService.createVariant(variantForm);
         return new ModelAndView("redirect:/teacher");
     }
